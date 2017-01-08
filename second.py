909dd59c5dd62f211a1a10166e365d1898865a3d
Vitor Enes Duarte <vitorenesduarte@gmail.com>
2017-01-08 11:46:46 +0000

    Run blast locally

--- second.py
+++ second.py
@@ -1,4 +1,5 @@
 from Bio.Blast import NCBIWWW
+from Bio.Blast.Applications import NcbiblastpCommandline
 import helper
 
 def blastp(protein, file_path):
@@ -8,6 +9,31 @@ def blastp(protein, file_path):
     """
     handle = NCBIWWW.qblast("blastp", "nr", protein)
     helper.write_blast(handle, file_path)
+
+def local_blastp(protein, file_path):
+    """
+    Corre o blast localmente para a proteína passada como argumento
+    e grava o resultado em file_path.
+    Assume a existência de uma base de dados local chamada
+    'swissprot'.
+    """
+
+    # Gravar a proteína num ficheiro
+    query_path = ".query"
+    helper.write_file(protein, query_path)
+
+    # Correr o blast
+    blastp_cline = NcbiblastpCommandline(
+        query=query_path,
+        db="swissprot",
+        evalue=10,
+        outfmt=5,
+        out=file_path)
+    blastp_cline()
+
+def extract_uniprot_id(hit_def):
+    parts = hit_def.split("|")
+    return parts[1]
 
 def main():
     ncbi_json_path = ".ncbi.json"
@@ -25,10 +51,32 @@ def main():
     p1_f = ".p1.xml"
     p2_f = ".p2.xml"
 
-    blastp("CCC", p1_f)
-    #blastp(p2, p2_f)
-    p1_r = helper.read_blast(p1_f)
-    print(p1_r)
+    #print("LOCAL")
+    #local_blastp(p1, p1_f)
+    #r = helper.read_blast(p1_f)
+    #for a in r.alignments:
+    #    uniprot_id = extract_uniprot_id(a.hit_def)
+    #    print(uniprot_id)
+
+    #print("\n\n\nWEB")
+    #blastp(p1, p1_f)
+    r = helper.read_blast(p1_f)
+    for a in r.alignments:
+        #print(a.hit_def)
+        #uniprot_id = extract_uniprot_id(a.hit_def)
+        #print(uniprot_id)
+
+
+        hsp = a.hsps[0]
+        print("- e:", hsp.expect)
+        print("- score:", hsp.score)
+        print("- align len:", hsp.align_length)
+        print("- matches:", hsp.identities)
+        print("- accession:", a.accession)
+        print("*", a.title)
+        print("*", a.hit_id)
+        print("*", a.length)
+        print("*", a.hit_def)
 
 if __name__ == "__main__":
     main()
