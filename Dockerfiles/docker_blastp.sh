4b7f4d3c2cbbaee88abca91af41120ff1db19112
Vitor Enes Duarte <vitorenesduarte@gmail.com>
2017-01-08 15:38:53 +0000

    Remove docker blast script and run only for one file instead of an entire folder

---
+++ Dockerfiles/docker_blastp.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+ENV_VARS=(
+  IN_FILE
+  OUT_FILE
+  DB
+)
+
+# Verificar que todas as variáveis de ambiente  necessárias estão configuradas.
+for ENV_VAR in "${ENV_VARS[@]}"
+do
+  if [ -z "${!ENV_VAR}" ]; then
+    echo ">>> ${ENV_VAR} is not configured; please export it."
+    exit 1
+  fi
+done
+
+$BLAST_BIN/blastp -query $IN_FILE -db $DB -outfmt 5 -out $OUT_FILE
+
