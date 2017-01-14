235780b26ef3036c3fed398e798721c02df69fa0
Vitor Enes Duarte <vitorenesduarte@gmail.com>
2017-01-14 19:30:41 +0000

    Move all site files to site folder

--- build-site.sh
+++ build-site.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-jupyter nbconvert --to html --template full index.ipynb
-
 cd site/
+
 for PAGE in $(ls *.ipynb); do
   jupyter nbconvert --to html --template full $PAGE
 done
-cd ../
 
 google-chrome index.html
+
