ac9becd864a7b1afd5650d65a5e0a19d574e814b
Vitor Enes Duarte <vitorenesduarte@gmail.com>
2017-01-15 23:03:53 +0000

    Add build site python script

---
+++ build-site.py
@@ -0,0 +1,18 @@
+import os
+import os.path
+
+site_path = "site"
+os.chdir(site_path)
+files = [f for f in os.listdir() if os.path.isfile(f)]
+
+for f in files:
+    if f.endswith(".ipynb"):
+        # para os ficheiros ipython notebook
+
+        if os.name == "nt":
+            cmd = "ipython"
+        elif os.name == "posix":
+            cmd = "jupyter"
+
+        # converter para html
+        os.system(cmd + " nbconvert --to html --template full " + f)
